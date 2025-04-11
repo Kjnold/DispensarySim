51f2aec6a79cd2e025fc6ee058a40d4dbd1a1834
Ketan Nold <ketannold@gmail.com>
2025-04-11 00:21:36 -0700

    Function Doors!
    doors open when -
    - the player is within 2f of the door
    - the player is looking at the doorknob
    - the player presses "E"
    - the door is unlocked
        - state changed by steps 1 & 2 followed by pressing "L"
    
    then slowly opens the door

--- Assets/Scripts/DoorDetector.cs
+++ Assets/Scripts/DoorDetector.cs
@@ -2,15 +2,51 @@ using UnityEngine;
 
 public class DoorDetector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private DoorOpener doorOpener;
     void Start()
     {
-        
+        doorOpener = GetComponentInParent<DoorOpener>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        DoorCheck();
+    }
+
+    /*
+    *   This method checks if the player is close enough to the door by comparing the distance between the door and the camera.
+    *   If the distance is less than 2 units, it returns true; otherwise, it returns false.
+    */
+    private bool CloseEnough(){
+        if(Vector3.Distance(transform.position, Camera.main.transform.position) < 2f){
+            return true;
+        }
+        return false;
+    }
+
+    /*
+    *   This method checks if the player is close enough to the door and if the doorOpener is not null.
+    *   If both conditions are met, it checks for key presses to toggle the door or change its lock state.
+    *   Key E toggles the door, and key L changes the lock state.
+    */
+    private void DoorCheck(){
+        if(CloseEnough() && LookingAtDoor() &&doorOpener != null){
+            if (Input.GetKeyDown(KeyCode.E)){   doorOpener.ToggleDoor();   }
+            else if(Input.GetKeyDown(KeyCode.L)){   doorOpener.ChangeLockState();   }
+        }
+    }
+
+    /*
+    *   This method checks if the player is looking at the door by casting a ray from the camera to the door.
+    *   If the ray hits the door, it returns true; otherwise, it returns false.
+    */
+    private bool LookingAtDoor(){
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit)){
+            if(hit.collider.gameObject == gameObject){
+                return true;
+            }
+        }
+        return false;
     }
 }
